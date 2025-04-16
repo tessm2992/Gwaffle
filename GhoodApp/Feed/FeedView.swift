04c98f612a8ab086a11b673bb565f0a2ae606235
tessm2992 <tessm2992@gmail.com>
2025-04-16 11:54:36 -0400

    Started Feed View

---
+++ GhoodApp/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  GhoodApp
+//
+//  Created by Tess Munsie on 4/16/25.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
