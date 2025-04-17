a3b206e9906ebcb64ad5fa70f698f626882a094d
tessm2992 <tessm2992@gmail.com>
2025-04-16 21:28:15 -0400

    updated feed post

---
+++ GhoodApp/Feed/ThreadPostView.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadPostView.swift
+//  GhoodApp
+//
+//  Created by Tess Munsie on 4/16/25.
+//
+
+import SwiftUI
+
+struct ThreadPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadPostView()
+}
