fa499c23201e17efca8166a228bda20259e95fd0
tessm2992 <tessm2992@gmail.com>
2025-04-22 16:04:55 -0400

    community detailed view

---
+++ GhoodApp/Communities/Views/CommunityCommentCell.swift
@@ -0,0 +1,83 @@
+//
+//  CommunityCommentCell.swift
+//  GhoodApp
+//
+//  Created by Tess Munsie on 4/22/25.
+//
+
+import SwiftUI
+
+struct CommunityCommentCell: View {
+    let ghoodPink: Color = Color(red: 255/255, green: 41/255, blue: 91/255)
+    
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            HStack(alignment: .top, spacing: 8) {
+                NavigationLink {
+                    ProfileView(showBackButton: true)
+                        .navigationBarBackButtonHidden()
+                } label: {
+                    Image("avatar")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40,height: 40)
+                        .clipShape(Circle())
+                }
+                VStack(alignment: .leading) {
+                    VStack(alignment: .leading,spacing: 4) {
+                        NavigationLink {
+                            ProfileView(showBackButton: true)
+                                .navigationBarBackButtonHidden()
+                        } label: {
+                            Text("tessm345")
+                                .font(.system(size: 13,weight: .semibold))
+                                .foregroundStyle(ghoodPink.opacity(0.7))
+                        }
+                        Text("body of comment")
+                            .font(.system(size: 12))
+                            .foregroundStyle(Color(.black))
+                            .padding(.top, 0)
+                    }
+                    .padding(10)
+                    .background(
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(Color(.systemGray6))
+                    )
+                    HStack(spacing: 9) {
+                        Text("2h")
+                            .font(.system(size: 11))
+                            .foregroundStyle(Color(.systemGray))
+                            .padding(.top, 2)
+                        Button(action: {},
+                               label: {
+                            Text("Like")
+                                .font(.system(size: 11,weight: .semibold))
+                                .foregroundStyle(Color(.systemGray))
+                                .padding(.top, 2)
+                        })
+                        Text("Reply")
+                            .font(.system(size: 11,weight: .semibold))
+                            .foregroundStyle(Color(.systemGray))
+                            .padding(.top, 2)
+                        Spacer()
+                        HStack(spacing: 3) {
+                            Text("3")
+                                .font(.system(size: 11))
+                                .foregroundStyle(Color(ghoodPink))
+                            Image(systemName: "hand.thumbsup.circle.fill")
+                                .resizable()
+                                .frame(width: 18,height: 18)
+                                .foregroundStyle(Color(ghoodPink))
+                        }
+                    }
+                }
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    CommunityCommentCell()
+}
--- GhoodApp/Communities/Views/DetailedCommunityView.swift
+++ GhoodApp/Communities/Views/DetailedCommunityView.swift
@@ -8,8 +8,89 @@
 import SwiftUI
 
 struct DetailedCommunityView: View {
+    private var ghoodPink: Color = Color(red: 255/255, green: 41/255, blue: 91/255)
+
+    @Environment(\.dismiss) private var dismiss
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            ScrollView {
+                VStack(alignment: .leading, spacing: 6) {
+                    HStack {
+                        NavigationLink {
+                            ProfileView(showBackButton: true)
+                                .navigationBarBackButtonHidden()
+                        } label: {
+                            Image("avatar")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 20,height: 20)
+                                .clipShape(Circle())
+                        }
+                        NavigationLink {
+                            ProfileView(showBackButton: true)
+                                .navigationBarBackButtonHidden()
+                        } label: {
+                            Text("tessm345")
+                                .font(.system(size: 13,weight: .semibold))
+                                .foregroundStyle(ghoodPink.opacity(0.7))
+                        }
+                        Text("12h")
+                            .font(.system(size: 13))
+                            .foregroundStyle(Color(.systemGray))
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                    .padding(.vertical,2)
+                    Text("placeholder title")
+                        .font(.system(size: 14,weight: .semibold))
+                        .foregroundStyle(Color(.black))
+                        .padding(.horizontal)
+                        .padding(.bottom, 0)
+                    Text("placeholder subtitle")
+                        .font(.system(size: 12))
+                        .foregroundStyle(Color(.black))
+                        .padding(.horizontal)
+                        .padding(.top, 0)
+                    HStack(spacing: 4) {
+                        Image(systemName: "hand.thumbsup.circle.fill")
+                            .resizable()
+                            .frame(width: 18,height: 18)
+                            .foregroundStyle(Color(ghoodPink))
+                        Text("24")
+                            .foregroundStyle(Color(ghoodPink))
+                        Spacer()
+                        Text("43 comments")
+                            .foregroundStyle(Color(ghoodPink))
+                    }
+                    .padding(.horizontal)
+                    .padding(.bottom)
+                    .font(.system(size: 13))
+                    ForEach(0 ..< 5, id: \.self) { index in
+                        CommunityCommentCell()
+                    }
+                }
+            }
+            .scrollIndicators(.hidden)
+            .foregroundStyle(Color(.systemGray5))
+            .toolbar{
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {dismiss()}, label: {
+                        Image(systemName: "arrow.left")
+                            .foregroundStyle(Color(ghoodPink))
+                            .fontWeight(.bold)
+                    })
+                }
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {dismiss()}, label: {
+                        Text("Tinx's Army")
+                            .foregroundStyle(Color(ghoodPink))
+                            .fontWeight(.bold)
+                    })
+                }
+            }
+        }
     }
 }
 
